13f6f43fba646601eaf1163d2a6b19b573548e71
Amber Zimmerman <ajz1221@gmail.com>
2022-03-10 20:21:51 -0700

    adding notes.js file

---
+++ Develop/api/notes.js
@@ -0,0 +1,39 @@
+const notesApi = require('express').Router();
+// require file that will help read and append files
+const { readAndAppend } = require('./helpers/fsUtils/')
+const uuid = require('../helpers/uuid');
+
+// Get route for retrieving all the notes
+notesApi.get('/', (req, res) =>
+    readFromFile('./db/feedback.json').then((data) => res.json(JSON.parse(data)))
+    );
+
+// Post route for submitting notes
+notesApi.post('/'. (req, res) => {
+    // need to destructure items in req.body
+    const { title, text } = req.body;
+
+    // check if all required properties are present with an if statement
+    if (title && text) {
+         // declare a variable that will be an object holding the notes
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+        };
+
+        // read and append new note to the json object
+        readAndAppend(newNote, './db/db.json');
+
+        const response = {
+        status: 'success',
+        body: newNote;
+        }
+
+        res.json(response);
+    } else {
+        res.json('Error in posting new note');
+    }
+});
+
+module.exports = notesApi;
